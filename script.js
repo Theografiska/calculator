9d2e50b237cabe571986a4f0b923a95653577f79
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-12-15 12:26:57 +0200

    Add functionality to calculate with decimal places

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 
 
-// addition:
+// operations:
 const addition = (firstNr, secondNr) => {
     return firstNr + secondNr;
 }
 
-// subtraction:
 const subtraction = (firstNr, secondNr) => {
     return firstNr - secondNr;
 }
 
-// multiplication:
 const multiplication = (firstNr, secondNr) => {
     return firstNr * secondNr;
 }
 
-// division:
 const division = (firstNr, secondNr) => {
     if (secondNr === 0) {
         return 'Error this, error that';
@@ -24,19 +21,20 @@ const division = (firstNr, secondNr) => {
     }
 }
 
+// main function:
 const operate = (numberOne, operator, numberTwo) => {
     switch (operator) {
         case '+':
-            return addition(numberOne, numberTwo);
+            return parseFloat(addition(numberOne, numberTwo)).toFixed(2);
             break;
         case '-':
-            return subtraction(numberOne, numberTwo);
+            return parseFloat(subtraction(numberOne, numberTwo)).toFixed(2);
             break;
         case 'x':
-            return multiplication(numberOne, numberTwo);
+            return parseFloat(multiplication(numberOne, numberTwo)).toFixed(2);
             break;
         case '/':
-            return division(numberOne, numberTwo);
+            return parseFloat(division(numberOne, numberTwo)).toFixed(2);
             break;
     }
 }
@@ -46,7 +44,7 @@ let operator = "";
 let secondNo = "";
 let operationResult = "";
 
-// div to hold all firstNo, operator, secondNo
+// div to hold all firstNo, operator, secondNo:
 const displayNumbers = document.querySelector("#display-numbers");
 
 const displayFirst = document.querySelector("#display-first");
@@ -56,52 +54,85 @@ const displayOperator = document.querySelector("#display-operator");
 // div to hold result:
 const displayResult = document.querySelector("#display-result");
 
-// number listener
-
+// number buttons' listener:
 const numberButtons = document.querySelectorAll(".number-button");
+
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
         if (operator === "" && firstNo === 0) {
-            firstNo = button.id;
-            displayFirst.textContent = firstNo;
+            firstNo = button.id; // first digit of first number
         } else if (operator === "" && firstNo !== 0) {
-            firstNo += button.id;
-            displayFirst.textContent = firstNo;
+            firstNo += button.id; // other digits of first number
         } else if (operator !== "" && secondNo === "") {
-            secondNo = button.id;
-            displaySecond.textContent = secondNo;
-        } else if (secondNo !== "") {
-            secondNo += button.id;
-            displaySecond.textContent = secondNo;
+            secondNo = button.id; // first digit of second number
+        } else if (secondNo !== "" && operationResult === "") {
+            secondNo += button.id; // other digits of second number
         }
+        displayFirst.textContent = firstNo;
+        displaySecond.textContent = secondNo;
     })
 })
 
-// operator listener
-
+// operator buttons and listener:
 const operatorButtons = document.querySelectorAll(".operator-button");
+
 operatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
         if (operator === "") {
             operator = button.id;
             displayOperator.textContent = operator;
         } else if (operator !== "" && operationResult !== "") {
-            firstNo = operationResult;
-            displayFirst.textContent = firstNo;
-            displayNumbers.style.color = "white";
-            displayNumbers.style.fontSize = "4rem";
-            operationResult = "";
-            displayResult.textContent = "";
+            operateAfterResult();
             operator = button.id;
             displayOperator.textContent = operator;
-            secondNo = "";
-            displaySecond.textContent = "";
-        } 
+        } else if (operator !== "" && secondNo !== "" && operationResult === "") {
+            operateAgainBeforeResult();
+            operator = button.id;
+            displayOperator.textContent = operator;
+        } else if (secondNo === "") {
+            operator = button.id;
+            displayOperator.textContent = operator;
+        }
     })
 })
 
-// clear listener:
+// continue operating before the result: 
 
+const operateAgainBeforeResult = () => {
+    getResult();
+    displayFirst.textContent = firstNo;
+
+    secondNo = "";
+    displaySecond.textContent = "";
+}
+
+// continue operating after the result:
+const operateAfterResult = () => {
+    firstNo = operationResult;
+    displayFirst.textContent = firstNo;
+
+    secondNo = "";
+    displaySecond.textContent = "";
+
+    displayReset(); 
+}
+
+// set operator:
+const setOperator = () => {
+    operator = button.id;
+    displayOperator.textContent = operator;
+}
+
+// reset display style: 
+
+const displayReset = () => {
+    displayNumbers.style.color = "white";
+    displayNumbers.style.fontSize = "4rem";
+    operationResult = "";
+    displayResult.textContent = "";
+}
+
+// clear button and listener:
 const clearButton = document.querySelector("#ac-button");
 
 clearButton.addEventListener("click", () => {
@@ -121,17 +152,19 @@ clearButton.addEventListener("click", () => {
 const resultsButton = document.querySelector("#results-button");
 
 resultsButton.addEventListener("click", () => {
-    if (operator !== "") {
-        firstNo = parseInt(firstNo);
-        secondNo = parseInt(secondNo);
-        operationResult = parseFloat(operate(firstNo, operator, secondNo).toFixed(2));
-        /*operationResult = operationResult.toFixed(2);
-        operationResult = parseFloat(operationResult);*/
+    if (operator !== "" && secondNo !== "") {
+        getResult();
         displayResult.textContent = operationResult;
         displayNumbers.style.color = "gray";
         displayNumbers.style.fontSize = "2rem";
     }
 })
+
+const getResult = () => {
+    firstNo = Number(parseFloat(firstNo).toFixed(2));
+    secondNo = Number(parseFloat(secondNo).toFixed(2));
+    operationResult = operate(firstNo, operator, secondNo);
+}
 
 /* problems: 
 
